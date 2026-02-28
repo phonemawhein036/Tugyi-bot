e6074490527e7f91c719a8483eb6b3703c1e4c18
phonemawhein036 <phonemawhein036@googlemail.com>
2026-02-28 16:56:58 +0630

    Update wireguard_service.py

--- services/wireguard_service.py
+++ services/wireguard_service.py
@@ -1,13 +1,15 @@
+from config import ENDPOINT, DNS
+
 def generate_wireguard_config(user_id):
     return f"""
 [Interface]
 PrivateKey = YOUR_PRIVATE_KEY
 Address = 172.16.0.{user_id % 250}/32
-DNS = 1.1.1.1
+DNS = {DNS}
 
 [Peer]
 PublicKey = YOUR_PUBLIC_KEY
 AllowedIPs = 0.0.0.0/0
-Endpoint = 162.159.192.10:500
+Endpoint = {ENDPOINT}
 PersistentKeepalive = 25
 """
